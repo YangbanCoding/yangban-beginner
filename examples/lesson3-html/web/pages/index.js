094efa1f7f6c526a15e5b10f42e34eb22de53b2f
yangbancoding <yangbancoding@gmail.com>
2020-11-18 12:39:04 +0900

    update example lesson1-4

--- examples/lesson3-html/web/pages/index.js
+++ examples/lesson3-html/web/pages/index.js
@@ -1,10 +1,19 @@
 function Index () {
   return (
     <div>
-      <div>html</div>
-      <div>html</div>
-      <div>html</div>
-      <div>html</div>
+      <div><div>다른 요소인 경우</div></div>
+      <div>텍스트인 경우</div>
+      <div></div>
+      <div />
+      <iframe 
+        width="560" 
+        height="315" 
+        src="https://www.youtube.com/embed/JPhLOCwlEQI" 
+        frameborder="0" 
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+        allowfullscreen
+      ></iframe>
+      <button onClick={() => alert('hi')}>버튼</button>
     </div>
   )
 }
--- examples/lesson4-css/web/pages/index.js
+++ examples/lesson4-css/web/pages/index.js
@@ -1,11 +1,75 @@
 function Index () {
   return (
     <div>
-      <div className='text1'>css</div>
+      <div className='text1'>텍스트1</div>
+      <div className='text2'>텍스트2</div>
+      <div className='text3'>텍스트3</div>
+      <div className='size64 yellow'>텍스트4</div>
+
+      <div className='flex start al-start'>
+        <div className='box'>박스</div>
+        <div className='box'>박스</div>
+        <div className='box'>박스</div>
+      </div>
+
+      <div className='flex center al-center'>
+        <div className='box'>박스</div>
+        <div className='box'>박스</div>
+        <div className='box'>박스</div>
+      </div>
+
+      <div className='flex end al-end'>
+        <div className='box'>박스</div>
+        <div className='box'>박스</div>
+        <div className='box'>박스</div>
+      </div>            
+
       <style jsx>{`
-      .text1 {
+      .size64 {
         font-size: 64px;
+      }
+      .yellow {
+        color: yellow;
+      }
+      .text1 {
+        font-size: 48px;
+        text-align: left;
         color: blue;
+      }
+      .text2 {
+        font-size: 32px;
+        text-align: center;
+      }
+      .text3 {
+        color: pink;
+      }
+      .flex {
+        display: flex;
+        height: 200px;
+        border: 1px solid black;
+      }
+      .box {
+        border: 1px solid black;
+        width: 100px;
+        height: 100px;
+      }
+      .start {
+        justify-content: flex-start;
+      }
+      .center {
+        justify-content: center;
+      }
+      .end {
+        justify-content: flex-end;
+      }
+      .al-start {
+        align-items: flex-start;
+      }
+      .al-center {
+        align-items: center;
+      }
+      .al-end {
+        align-items: flex-end;
       }
       `}</style>
     </div>
