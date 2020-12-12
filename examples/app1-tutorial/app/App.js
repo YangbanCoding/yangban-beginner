1ca5e2a33a41bdeb08250d482a32a3f2cd2f3385
yangbancoding <yangbancoding@gmail.com>
2020-12-12 12:20:58 +0900

    app1 added

---
+++ examples/app1-tutorial/app/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function App () {
+  return (
+    <View>
+      <Text>안녕하세요 양반코딩입니다</Text>
+    </View>
+  )
+}
+
+export default App
